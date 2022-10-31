8597fcda87b63ecdec3c128e59d3dbc85207cbdb
root <root@LAPTOP-DQ6DD8C6.localdomain>
2022-10-31 18:36:13 +0530

    12. Cancel Order

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
 import "hardhat/console.sol";
 import "./Token.sol";
 
@@ -8,75 +9,153 @@ contract Exchange {
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
-    uint256 public ordersCount;
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
-    event Deposit(address token, address user, uint256 amount, uint256 balance);
-    event Withdraw(address token, address user, uint256 amount, uint256 balance);
-
-    event Order ( 
+    event Deposit(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
+    event Order(
         uint256 id,
-        address user, 
-        address tokenGet,  
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
         uint256 amountGet,
         address tokenGive,
         uint256 amountGive,
         uint256 timestamp
     );
 
-    struct _Order { //all of this get stored in the blockchain
-        uint256 id; //unique identifier for the transaction
-        address user; //user who made the order
-        address tokenGet;   
-        uint256 amountGet;
-        address tokenGive;
-        uint256 amountGive;
-        uint256 timestamp;
+
+    struct _Order {
+        // Attributes of an order
+        uint256 id; // Unique identifier for order
+        address user; // User who made order
+        address tokenGet; // Address of the token they receive
+        uint256 amountGet; // Amount they receive
+        address tokenGive; // Address of token they give
+        uint256 amountGive; // Amount they give
+        uint256 timestamp; // When order was created
     }
+
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
-    //Deposit Tokens
+
+
+    // ------------------------
+    // DEPOSIT & WITHDRAW TOKEN
+
     function depositToken(address _token, uint256 _amount) public {
-        //Transfer tokens to exchange
+        // Transfer tokens to exchange
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
-        //Update user balance
+
+        // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
-        //Emit an event
+
+        // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
-    //Withdraw Token
+
     function withdrawToken(address _token, uint256 _amount) public {
-        //Ensure user has enough token to Withdraw
+        // Ensure user has enough tokens to withdraw
         require(tokens[_token][msg.sender] >= _amount);
-        //Transfer tokens to user
+
+        // Transfer tokens to user
         Token(_token).transfer(msg.sender, _amount);
-        //Update user balance 
+
+        // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
-        //Emit an Event
+
+        // Emit event
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
-    //Check Balance 
-    function balanceOf(address _token, address _user) 
-    public 
-    view 
-    returns (uint256) {
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
         return tokens[_token][_user];
     }
-    //MAKE AND CANCEL ORDERS
-     
-     //Token give -> the token they want to spend
-     //Token get -> the token they want to receive
-     function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
 
+
+    // ------------------------
+    // MAKE & CANCEL ORDERS
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        // Prevent orders if tokens aren't on exchange
         require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
 
-        ordersCount = ordersCount + 1;
+        // Instantiate a new order
+        orderCount = orderCount + 1;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
 
-        _Order(ordersCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+        // Emit event
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+    }
 
-        emit Order(ordersCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
-     }
+    function cancelOrder(uint256 _id) public {
+        // Fetch order
+        _Order storage _order = orders[_id];
+
+        // Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+
+        // Order must exist
+        require(_order.id == _id);
+
+        // Cancel the order
+        orderCancelled[_id] = true;
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
+
 }
-
--- test/Exchange.js
+++ test/Exchange.js
@@ -22,6 +22,7 @@ describe('Exchange', () =>{
         deployer = accounts[0]
         feeAccount = accounts[1]
         user1 = accounts[2]
+        user2 = accounts[3]
 
         let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100))
         exchange = await Exchange.deploy(feeAccount.address, feePercent)
@@ -156,7 +157,7 @@ describe('Exchange', () =>{
         })
 
         it('Tracks newly created orders', async () => {
-            expect(await exchange.ordersCount()).to.equal(1)
+            expect(await exchange.orderCount()).to.equal(1)
         })
 
         it('Emits an Order event', async () => {
@@ -178,5 +179,72 @@ describe('Exchange', () =>{
             expect(exchange.connect(user1).makeOrder(token2.address, tokens(1), token1.address, tokens(1))).to.be.reverted
         })
     })
+  })
+
+  describe('Order Actions', async () => {
+    let transaction, result
+    let amount = tokens(1)
+
+    beforeEach(async () => {
+        //Approve Token
+        transaction = await token1.connect(user1).approve(exchange.address, amount)
+        result = await transaction.wait()
+        //Deposit Token
+        transaction = await exchange.connect(user1).depositToken(token1.address, amount)            
+        result = await transaction.wait()
+        //Make Order
+        transaction = await exchange.connect(user1).makeOrder(token2.address, tokens(1), token1.address, tokens(1))
+        result = await transaction.wait()
+      })
+
+    describe('Cancelling Orders', async () => {
+
+        describe('Success', async () => {
+            beforeEach(async () => {
+                transaction = await exchange.connect(user1).cancelOrder(1);
+                result = await transaction.wait();
+            })
+            it('Updates Cancelled Orders', async () => {
+                expect(await exchange.orderCancelled(1)).to.equal(true)
+            })
+            it('Emits a Cancel event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Cancel')
+    
+                const args = event.args
+                expect(args.id).to.equal(1)
+                expect(args.user).to.equal(user1.address)
+                expect(args.tokenGet).to.equal(token2.address)
+                expect(args.amountGet).to.equal(tokens(1))
+                expect(args.tokenGive).to.equal(token1.address)
+                expect(args.amountGive).to.equal(tokens(1))
+                expect(args.timestamp).to.at.least(1)
+            }) 
+        })
+
+        describe('Failure', async () => {
+          beforeEach(async () => {
+        //Approve Token
+        transaction = await token1.connect(user1).approve(exchange.address, amount)
+        result = await transaction.wait()
+        //Deposit Token
+        transaction = await exchange.connect(user1).depositToken(token1.address, amount)            
+        result = await transaction.wait()
+        //Make Order
+        transaction = await exchange.connect(user1).makeOrder(token2.address, amount , token1.address, amount)
+        result = await transaction.wait()
+          })
+            it('Rejects Invalid Order ID', async () => {
+              const invalidOrderID = 99999
+               await expect(exchange.connect(user1).cancelOrder(invalidOrderID)).to.be.reverted
+            })
+            it('Rejects Unauthorized Cancellations', async () => {
+              await expect(exchange.connect(user2).cancelOrder(1)).to.be.reverted
+            }) 
+
+        })
+
+    })
+
   })
 })
