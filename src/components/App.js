7c301311618abd58faafb25fec5360b3cb844358
root <root@LAPTOP-DQ6DD8C6.localdomain>
2022-11-08 22:53:00 +0530

    20. Deposits

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 function App() {
   const dispatch = useDispatch()
@@ -41,7 +43,10 @@ function App() {
 
     // Load Exchange Contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to Events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => {
@@ -58,7 +63,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
