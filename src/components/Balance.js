adf2a2124f80644a07d22381c05df32772a72d34
root <root@LAPTOP-DQ6DD8C6.localdomain>
2022-11-09 17:08:35 +0530

    22. Withdrawals

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -61,6 +61,17 @@ const Balance = () => {
     }
   }
 
+  const withdrawHandler = (e, token) => {
+    e.preventDefault()
+    if (token.address === tokens[0].address) {
+      transferTokens(provider, exchange, 'Withdraw', token, token1TransferAmount, dispatch)
+      setToken1TransferAmount(0)
+    } else {
+      transferTokens(provider, exchange, 'Withdraw', token, token2TransferAmount, dispatch)
+      setToken2TransferAmount(0)
+    }
+  }
+
   useEffect(() => {
     if(exchange && tokens[0] && tokens[1] && account) {
       loadBalances(exchange, tokens, account, dispatch)
@@ -86,7 +97,7 @@ const Balance = () => {
           <p><small>Exchange</small><br />{exchangeBalances && exchangeBalances[0]}</p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[0])}>
+        <form onSubmit={isDeposit ? (e) => depositHandler(e, tokens[0]) : (e) => withdrawHandler(e, tokens[0])}>
           <label htmlFor="token0">{symbols && symbols[0]} Amount</label>
           <input
             type="text"
@@ -116,7 +127,7 @@ const Balance = () => {
           <p><small>Exchange</small><br />{exchangeBalances && exchangeBalances[1]}</p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[1])}>
+        <form onSubmit={isDeposit ? (e) => depositHandler(e, tokens[1]) : (e) => withdrawHandler(e, tokens[1])}>
           <label htmlFor="token1"></label>
           <input 
           type="text" 
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -55,7 +55,12 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Deposit', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
+
+  exchange.on('Withdraw', (token, user, amount, balance, event) => {
+    dispatch({ type: 'TRANSFER_SUCCESS', event })
+  })
 }
+
 
 // ------------------------------------------------------------------------------
 // LOAD USER BALANCES (WALLET & EXCHANGE BALANCES)
@@ -88,10 +93,18 @@ export const transferTokens =  async (provider, exchange, transferType, token, a
     const signer = await provider.getSigner()
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
 
-    transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
-    await transaction.wait()
-    transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    if (transferType === 'Deposit') {
 
+      transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+      await transaction.wait()
+      transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)  
+    
+    } else {
+      
+      transaction = await exchange.connect(signer).withdrawToken(token.address, amountToTransfer)
+   
+    }
+   
     await transaction.wait()
 
   } catch(error) {
